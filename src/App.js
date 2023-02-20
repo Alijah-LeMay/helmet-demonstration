8de12942f21d22a333b6ce3b68d63edf572cdf09
alijah-tectonics <alijahlemay12@gmail.com>
2023-02-19 19:16:34 -0800

    thanks

--- src/App.js
+++ src/App.js
@@ -7,17 +7,15 @@ function App() {
       <Helmet>
         <meta charSet='utf-8' />
         <title>Helmet Tutorial</title>
-
         <meta
           name='description'
           content='React-Helmet demo by the web developers.'
         />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-
-        <meta name='theme-color' content='#010101' />
       </Helmet>
       <div className='App'>
         <h2>Hello and welcome to the React-Helmet demo</h2>
+        <p>Thanks for watching</p>
       </div>
     </>
   )
