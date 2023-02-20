376dfdeb18f316f9aa959a93ad9dd48b69264350
alijah-tectonics <alijahlemay12@gmail.com>
2023-02-19 19:13:34 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Helmet } from 'react-helmet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Helmet Tutorial</title>
+
+        <meta
+          name='description'
+          content='React-Helmet demo by the web developers.'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+
+        <meta name='theme-color' content='#010101' />
+      </Helmet>
+      <div className='App'>
+        <h2>Hello and welcome to the React-Helmet demo</h2>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
